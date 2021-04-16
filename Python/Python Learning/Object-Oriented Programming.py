263aaf4db094b3a047e1f8113c76fd085d140fa2
Pranay845 <pranaypenta845@gmail.com>
2021-04-16 14:27:59 +0530

    Started OOPs

--- Python/Python Learning/Object-Oriented Programming.py
+++ Python/Python Learning/Object-Oriented Programming.py
@@ -1,14 +1,71 @@
-# Object-oriented programming
-""""""
-
 # Topic: Programming paradigms
-""""""
+"""
+Python is a multi-paradigm programming language.
+It supports different programming approaches.
+Programming paradigm is an approach to solve problem using some programming language
+or also we can say it is a method to solve a problem using tools and techniques that
+are available to us following some approach.
+There are lots for programming language that are known
+but all of them need to follow some strategy when
+they are implemented and this methodology/strategy is paradigms.
+"""
+
+# SubTopic: Object-oriented programming
+"""
+Any object in real life has characteristics.
+This is also illustrated in programming languages
+One of the popular approaches to solve a programming problem is by creating objects.
+This is known as Object-Oriented Programming (OOP).
+An object has two characteristics:
+    1. attributes
+    2. behavior
+
+Let's take an example:
+A parrot is an object, as it has the following properties:
+    1. name, age, color as attributes
+    2. singing, dancing as behavior
+    
+The concept of OOP in Python focuses on creating reusable code.
+This concept is also known as DRY (Don't Repeat Yourself).
+"""
 
 # Topic: Class
-""""""
+"""
+A class is a blueprint or a design for a object that defines the it's attributes (variables in OOPs) and
+the methods (Functions in OOPs) common to all objects or instances  of a same design.
+"""
+
+# SubTopic: __init__ method
+"""
+The __init__ method is a special method which is executed every time automatically
+when a object of it's class is created and executed.
+This is similar to constructors in other languages.
+
+Constructors are used to initialize or
+create attributes/variables in a class for the object and
+also these are responsible for the allocation of the object created by the class in the memory.
+We can also change the variables inside the class in outside of the class.
+These attributes created by the init method can be used inside the class.
+
+In every method of class a argument is passed called self.
+self is a pointer to determine based on which object is calling a specific method or referring to what in a class.
+
+These method is useful when you need to create attributes which can be used by other methods in class.
+"""
+
+# Note: When we want compare two objects we create a compare function inside the class and pass the two arguments
+#  one is which object is calling and the other is which object needs to be compared.
+#  The argument need not to be the name of the object which is compared.
+#  Then we use if statement and the compare method to compare objects
+#  because python does not know how to compare objects.
 
 # SubTopic: Types of attributes (Variables)
-""""""
+"""
+There are three different types of variables in OOPs in python:
+    1. Instance variable (Object level variable)
+    2. Static variable (Class level variable)
+    3. Local variable (Method level variable)
+"""
 
 # SubTopic: Types of methods (Functions)
 """"""
